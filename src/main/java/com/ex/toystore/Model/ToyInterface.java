279f8e9972059916beefd356bb9c511c0097f256
Daiza <salkodg@gmail.com>
2023-09-02 11:18:24 +0500

    Попытка 1. Удалил ненужный интерфейс

--- src/main/java/com/ex/toystore/Model/ToyInterface.java
+++
@@ -1,6 +0,0 @@
-package com.ex.toystore.Model;
-
-public interface ToyInterface {
-    String RegExValidNum = "^[0-9]*$";
-    String RegExValidName = "^[А-Яа-яЁёa-zA-Z][А-Яа-яЁёa-zA-Z0-9]{2,128}$";
-}
